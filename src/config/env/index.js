c80a7d6ff56202038d9f3d1e573d5efc55e726ea
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-09-20 17:22:37 +0100

    Create environment file loader

---
+++ src/config/env/index.js
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+
+const getEnvironmentFileName = environment => {
+    const files = {
+        development: 'development.env',
+        production: 'production.env'
+    };
+    return files[environment];
+};
+
+const environment = process.env.NODE_ENV;
+
+dotenv.config({ path: `./src/config/env/${getEnvironmentFileName(environment)}` });
