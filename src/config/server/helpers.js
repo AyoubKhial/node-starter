d716067821f25f44d9bebf86308f847e006c72ea
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-09-23 00:50:35 +0100

    Add helpers file with getPathsList fn to extract path by module

---
+++ src/config/server/helpers.js
@@ -0,0 +1,13 @@
+const getPathsList = node => {
+    const pathsList = [];
+    for (const [key, value] of Object.entries(node)) {
+        if (value && typeof value === 'object') {
+            pathsList.push(...getPathsList(value).map(p => `${key}/${p}`));
+        } else {
+            pathsList.push(key);
+        }
+    }
+    return pathsList;
+};
+
+export default getPathsList;
