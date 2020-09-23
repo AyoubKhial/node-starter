1d3ac981f1c1bb989c7f015b6765f4a7f7194cd3
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-09-24 00:43:35 +0100

    Add error handler middleware

---
+++ src/middleware/error-handler.js
@@ -0,0 +1,26 @@
+import ErrorResponse from '../utils/error-response';
+import Response from '../utils/response-builder';
+
+const errorHandler = (err, req, res, next) => {
+    // Mongoose bad ObjectID
+    if (err.name === 'CastError') {
+        const message = 'Resource not found.';
+        err = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered.';
+        err = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(error => error.message);
+        err = new ErrorResponse(message, 400);
+    }
+
+    Response.build(res, { success: false, error: err.message || 'Server Error.' }, err.statusCode || 500);
+};
+
+export default errorHandler;
