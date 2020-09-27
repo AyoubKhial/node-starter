021551a69f77513c6c7eef62fbd16efd10b27a55
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-09-27 15:41:20 +0100

    Add JWT environment variables

--- src/config/env-vars.js
+++ src/config/env-vars.js
@@ -3,7 +3,10 @@ const config = {
     NODE_PORT: process.env.NODE_PORT,
     DB_HOST: process.env.DB_HOST,
     DB_PORT: process.env.DB_PORT,
-    DB_NAME: process.env.DB_NAME
+    DB_NAME: process.env.DB_NAME,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRE: process.env.JWT_EXPIRE,
+    JWT_COOKIE_EXPIRE: process.env.JWT_COOKIE_EXPIRE
 };
 
 export default config;
