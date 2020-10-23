f493ad759a753dbd6c3d6d098a91e48aa8ae6300
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-10-23 02:59:21 +0100

    Bind the new added middlewares to specific routes

--- src/utils/http-binder.js
+++ src/utils/http-binder.js
@@ -1,4 +1,6 @@
 const advancedResult = require('../middleware/advanced-result');
+const clearCache = require('../middleware/clear-cache');
+const cachedResult = require('../middleware/cached-result');
 const protect = require('../middleware/auth');
 
 const insert = (arr, index, newItem) => [...arr.slice(0, index), newItem, ...arr.slice(index)];
@@ -13,7 +15,9 @@ const binder = (app, routes) => {
     for (const route of routes) {
         let args = [route.path, route.handler];
         if (route?.protected) args = addMiddleware(args, protect(route?.protected?.roles));
+        if (route?.cachedResult) args = addMiddleware(args, cachedResult(route?.cachedResult));
         if (route?.advancedResult) args = addMiddleware(args, advancedResult(route?.advancedResult?.model));
+        if (route?.clearCache) args = addMiddleware(args, clearCache(route?.clearCache));
         app[route.method.toLowerCase()](...args);
     }
     return app;
