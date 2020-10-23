5390d38a43fc9fbbde816507cbeda5775d9f2586
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-10-23 02:52:39 +0100

    Add cache helper module

---
+++ src/config/cache/helper.js
@@ -0,0 +1,22 @@
+const util = require('util');
+const { client } = require('./index');
+
+const set = ({ key, data, mode, expiresIn }) => {
+    client.set(key, data, mode, expiresIn);
+};
+
+const get = key => {
+    client.get = util.promisify(client.get);
+    return client.get(key);
+};
+
+const remove = key => {
+    client.del(key);
+};
+
+const getKeys = keyPattern => {
+    client.keys = util.promisify(client.keys);
+    return client.keys(keyPattern);
+};
+
+module.exports = { set, get, remove, getKeys };
