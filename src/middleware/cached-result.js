af6c3ba6546601057766a7c676d68cd7d1ea633d
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-10-23 02:55:51 +0100

    Add middleware to return cached data

---
+++ src/middleware/cached-result.js
@@ -0,0 +1,15 @@
+const cache = require('../config/cache/helper');
+
+const cachedResult = ({ collection, method, type, keys, source }) => async (req, res, next) => {
+    const origin = source === 'query' ? req.query : source === 'body' ? req.body : req.params;
+    const data = { collection, method, type };
+    const keysValues = keys.reduce((target, current) => ({ ...target, [current]: origin[current] }), {});
+    if ('page' in keysValues && !keysValues.page) keysValues.page = 1;
+    if ('size' in keysValues && !keysValues.size) keysValues.size = 100;
+    const dataAsString = Object.entries({ ...data, ...keysValues }).reduce((a, b) => `${a} ${b[0]}:${b[1]}`, '');
+    const cachedData = await cache.get(dataAsString.trim());
+    if (cachedData !== null) res.cachedData = JSON.parse(cachedData);
+    next();
+};
+
+module.exports = cachedResult;
