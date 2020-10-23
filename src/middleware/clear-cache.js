9da120b8069ee7c418d5d55eb61a5733d4090a6d
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-10-23 23:55:40 +0100

    Use map instead of for

--- src/middleware/clear-cache.js
+++ src/middleware/clear-cache.js
@@ -3,11 +3,11 @@ const cache = require('../config/cache/helper');
 const clearCache = ({ collections, methods, types }) => async (req, res, next) => {
     const data = [collections, methods, types];
     const combinations = data.reduce((a, b) => a.reduce((r, v) => r.concat(b.map(w => [].concat(v, w))), []));
-    const promises = [];
-    for (const combination of combinations) {
+    const promises = combinations.map(combination => {
         const keyPattern = `collection:${combination[0]} method:${combination[1]} type:${combination[2]}*`;
-        promises.push(cache.getKeys(keyPattern));
-    }
+        return cache.getKeys(keyPattern);
+    });
+    console.log(promises);
     const keys = await Promise.all(promises);
     for (const key of keys.flat()) {
         cache.remove(key);
