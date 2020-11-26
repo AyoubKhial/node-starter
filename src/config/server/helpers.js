0a8e1701471cef8c5f9ac51f456207f9711ae84b
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-11-26 15:45:36 +0100

    Refactor get pathPathsList helper fujnction

--- src/config/server/helpers.js
+++ src/config/server/helpers.js
@@ -1,11 +1,8 @@
 const getPathsList = node => {
     const pathsList = [];
     for (const [key, value] of Object.entries(node)) {
-        if (value && typeof value === 'object') {
-            pathsList.push(...getPathsList(value).map(p => `${key}/${p}`));
-        } else {
-            pathsList.push(key);
-        }
+        if (value && typeof value === 'object') pathsList.push(...getPathsList(value).map(p => `${key}/${p}`));
+        else pathsList.push(key);
     }
     return pathsList;
 };
