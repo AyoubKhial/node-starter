cc7c922fb4f617ecbabc9dafe7cc222927e5da1b
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-11-27 20:54:25 +0100

    Add unit test to async wrapper utility

---
+++ tests/unit/utils/async-wrapper.spec.js
@@ -0,0 +1,16 @@
+const asyncWrapper = require('utils/async-wrapper');
+
+describe('Async wrapper utility', () => {
+    it('Should return a successful promise data.', async () => {
+        const promiseFunction = () => Promise.resolve({ data: 'some data' });
+        const promise = await asyncWrapper(promiseFunction)({});
+        expect(promise).toEqual({ data: 'some data' });
+    });
+
+    it('Should call the next function.', async () => {
+        const next = jest.fn();
+        const promiseFunction = () => Promise.reject()
+        await asyncWrapper(promiseFunction)({ next });
+        expect(next).toHaveBeenCalled();
+    });
+})
