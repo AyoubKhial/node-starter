769ec3e750ed615e9b8653115b8272d08f901ae2
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-11-27 22:00:17 +0100

    Add return statements to protect middlware

--- src/middleware/protect.js
+++ src/middleware/protect.js
@@ -1,18 +1,18 @@
 const protect = ({ roles, userModel, config, verifyToken }) => async (req, res, next) => {
     let token;
     const authorization = req?.headers?.authorization;
-    if (authorization && authorization.startsWith('Bearer')) token = authorization.split(' ')[1];
+    if (authorization?.startsWith('Bearer')) token = authorization.split(' ')[1];
     else if (req?.cookies?.token) token = req?.cookies?.token;
-    if (!token) next({ message: 'Not authorized to access this route', code: 401 });
+    if (!token) return next({ message: 'Not authorized to access this route', code: 401 });
     try {
-        const decoded = verifyToken(token, config.jwt.secret);
+        const decoded = verifyToken(token, config?.jwt?.secret);
         req.user = await userModel.findById(decoded.id);
         if (!roles.includes(req?.user?.role)) {
-            next({ message: `User with role '${req?.user?.role}' is not authorized to access this route.`, code: 403 });
+            return next({ message: `User with role '${req?.user?.role}' is not authorized to access this route.`, code: 403 });
         }
-        next();
+        return next();
     } catch (err) {
-        next({ message: 'Not authorized to access this route', code: 401 });
+        return next({ message: 'Not authorized to access this route', code: 401 });
     }
 };
 
