df7c7167b7ba8477314c7696d0f422ee4e2abc48
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-11-27 19:54:01 +0100

    Add unit test to 'await to' utility

---
+++ tests/unit/utils/await-to.spec.js
@@ -0,0 +1,16 @@
+const to = require('utils/await-to');
+
+describe('Await to utility', () => {
+    it('Should return data when a promise is resolved.', async () => {
+        const promise = Promise.resolve({ data: 'some data' });
+        const [err, data] = await to(promise);
+        expect(err).toBeNull();
+        expect(data).toEqual({ data: 'some data' });
+    });
+    it('Should return error when promise is rejected.', async () => {
+        const promise = Promise.reject({ data: 'some data' });
+        const [err, data] = await to(promise);
+        expect(data).toBeUndefined();
+        expect(err).toEqual({ data: 'some data' });
+    });
+})
