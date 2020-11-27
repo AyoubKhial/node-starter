6026e2e3be6a4f81af4bdb6304ec6e7a1ca93df6
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-11-27 22:56:58 +0100

    Add unit test for rate limiter middleware

---
+++ tests/unit/middleware/rate-limiter.spec.js
@@ -0,0 +1,11 @@
+const rateLimiter = require('middleware/rate-limiter');
+
+describe('Protect middleware', () => {
+    it('Should check that token does not exist.', async () => {
+        const rateLimit = options => options;
+        const extraOptions = { windowMs: 1 * 60 * 1000, max: 5 }
+        const response = rateLimiter({ rateLimit, extraOptions });
+        expect(response).toHaveProperty('message');
+        expect(response.message).toEqual({ success: false, error: 'You have exceeded the 5 requests in 1 minutes limit!' });
+    });
+});
