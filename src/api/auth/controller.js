152b236b39d4b1c6ca1acd230d636256c23f2f1d
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-11-29 00:24:27 +0100

    Refactore auth controller

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -8,18 +8,18 @@ const register = asyncWrapper(async ({ req, res, next, userModel, service }) => 
 
 const login = asyncWrapper(async ({ req, res, next, userModel, service }) => {
     const { email, password } = req.body;
-    if (!email || !password) return next({ message: 'Please provide an email and password', code: 400 });
+    if (!email || !password) return next({ message: 'Please provide an email and password.', code: 400 });
     const user = await userModel.findOne({ email }).select('+password');
-    if (!user) return next({ message: 'Invalid credentials', code: 401 });
+    if (!user) return next({ message: 'Invalid credentials.', code: 401 });
     const isMatched = await user.matchPassword(password);
-    if (!isMatched) return next({ message: 'Invalid credentials', code: 401 });
+    if (!isMatched) return next({ message: 'Invalid credentials.', code: 401 });
     return service.sendTokenResponse(user, 200, res);
 });
 
-const logout = asyncWrapper(async ({ req, res, next, response }) => {
+const logout = ({ req, res, next, response }) => {
     res.cookie('token', 'none', { expires: new Date(Date.now() + 10 * 1000) });
     return response.build(res, { success: true }, 200);
-});
+};
 
 const getLoggedInUser = ({ req, res, next, response }) => {
     const user = req.user;
@@ -28,8 +28,9 @@ const getLoggedInUser = ({ req, res, next, response }) => {
 
 const forgotPassword = asyncWrapper(async ({ req, res, next, response, userModel, mailerService, externalMailService, config }) => {
     const email = req.body.email;
+    if (!email) return next({ message: 'Please provide an email.', code: 400 });
     const user = await userModel.findOne({ email });
-    if (!user) return next({ message: 'There is no user with that email', code: 404 });
+    if (!user) return next({ message: `There is no user with '${email}' email.`, code: 404 });
     const resetToken = user.getResetPasswordToken();
     await user.save({ validateBeforeSave: false });
     const resetUrl = `${req.protocol}://${req.get('host')}/api/auth/reset-password/${resetToken}`;
@@ -47,10 +48,12 @@ const forgotPassword = asyncWrapper(async ({ req, res, next, response, userModel
 
 const resetPassword = asyncWrapper(async ({ req, res, next, userModel, service, crypto }) => {
     const resetToken = req.params.resetToken;
+    if (!resetToken) return next({ message: 'Please provide the reset token.', code: 400 });
     const password = req.body.password;
+    if (!password) return next({ message: 'Please provide the new password.', code: 400 });
     const resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
     const user = await userModel.findOne({ resetPasswordToken, resetPasswordExpire: { $gt: Date.now() } });
-    if (!user) return next({ message: 'Invalid Token', code: 401 });
+    if (!user) return next({ message: 'Invalid Token.', code: 401 });
     user.password = password;
     user.resetPasswordToken = undefined;
     user.resetPasswordExpire = undefined;
