3569d021802464d7ba57b1802b4ddc88b9732fa2
Ayoub KHIAL <ayouub.khial@gmail.com>
2020-11-28 02:08:26 +0100

    Use optional chaining in mailer service

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,17 +1,17 @@
 const sendMail = ({ options, config, service }) => {
     const transporter = service.createTransport({
-        host: config.mail.host,
-        port: config.mail.port,
+        host: config?.mail?.host,
+        port: config?.mail?.port,
         auth: {
-            user: config.mail.email,
-            pass: config.mail.password
+            user: config?.mail?.email,
+            pass: config?.mail?.password
         }
     });
     const message = {
-        from: `${config.mail.fromName} <${config.mail.fromEmail}>`,
-        to: options.email,
-        subject: options.subject,
-        text: options.message
+        from: `${config?.mail?.fromName} <${config?.mail?.fromEmail}>`,
+        to: options?.email,
+        subject: options?.subject,
+        text: options?.message
     };
     return transporter.sendMail(message);
 };
